f75e8f97372e3829fecf870ff34c9ee77d508318
utkarsh-khalkar <utkarsh.khalkar@gmail.com>
2024-06-04 00:43:58 +0530

    Added Dockerfile, Kubernetes manifests, and GitHub Actions workflow

---
+++ main.py
@@ -0,0 +1,7 @@
+from flask import Flask 
+app = Flask(__name__) 
+@app.route('/') 
+def hello_world(): 
+    return 'Hello, World! This is the Wisecow application.' 
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=80) 
